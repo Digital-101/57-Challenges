229146c08eb4b4f81015288f3ce014862f2ce25a
Mdubane Digits <65094648+Digital-101@users.noreply.github.com>
2025-03-06 14:00:41 +0200

    Update magic8Ball.cs

--- 57/DataStructures/magic8Ball.cs
+++ 57/DataStructures/magic8Ball.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace _57.DataStructures
 {
-    internal class magic8Ball
+    class Magic8Ball
+{
+    static void Main()
     {
+        // List of possible responses
+        string[] responses = {
+            "Yes",
+            "No",
+            "Maybe",
+            "Ask again later"
+        };
+
+        // Prompt the user for a question
+        Console.Write("What's your question? ");
+        string question = Console.ReadLine();
+
+        // Create a random number generator
+        Random rand = new Random();
+
+        // Generate a random index to select a response
+        int index = rand.Next(responses.Length);
+
+        // Display the chosen response
+        Console.WriteLine(responses[index]);
     }
+}
+
 }
