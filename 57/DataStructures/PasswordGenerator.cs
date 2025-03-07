56cd14bce72fad111ff78597fdfa4e4a7929cf0b
Mdubane Digits <65094648+Digital-101@users.noreply.github.com>
2025-03-07 12:30:17 +0200

    Update PasswordGenerator.cs

--- 57/DataStructures/PasswordGenerator.cs
+++ 57/DataStructures/PasswordGenerator.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace _57.DataStructures
 {
-    internal class PasswordGenerator
+    class PasswordGenerator
+{
+    static Random random = new Random();
+
+    static void Main()
     {
+        Console.Write("What's the minimum length? ");
+        int minLength = int.Parse(Console.ReadLine());
+
+        Console.Write("How many special characters? ");
+        int specialCharCount = int.Parse(Console.ReadLine());
+
+        Console.Write("How many numbers? ");
+        int numberCount = int.Parse(Console.ReadLine());
+
+        string password = GeneratePassword(minLength, specialCharCount, numberCount);
+        Console.WriteLine("\nYour password is: " + password);
     }
+
+    static string GeneratePassword(int minLength, int specialCharCount, int numberCount)
+    {
+        string letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string numbers = "0123456789";
+        string specialChars = "!@#$%^&*()_-+=<>?";
+
+        List<char> password = new List<char>();
+
+        // Add special characters
+        for (int i = 0; i < specialCharCount; i++)
+            password.Add(specialChars[random.Next(specialChars.Length)]);
+
+        // Add numbers
+        for (int i = 0; i < numberCount; i++)
+            password.Add(numbers[random.Next(numbers.Length)]);
+
+        // Add remaining letters to meet min length
+        int remainingChars = Math.Max(minLength - password.Count, 0);
+        for (int i = 0; i < remainingChars; i++)
+            password.Add(letters[random.Next(letters.Length)]);
+
+        // Shuffle the password to randomize order
+        password = password.OrderBy(x => random.Next()).ToList();
+
+        return new string(password.ToArray());
+    }
+}
+
 }
