e8982e08804c89a82e6486b6ec67ebbcf043ac7f
Mdubane Digits <65094648+Digital-101@users.noreply.github.com>
2025-03-07 10:21:27 +0200

    Update EmployeeListRemoval.cs

--- 57/DataStructures/EmployeeListRemoval.cs
+++ 57/DataStructures/EmployeeListRemoval.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace _57.DataStructures
 {
-    internal class EmployeeListRemoval
+    class EmployeeList
+{
+    static void Main()
     {
+        // List of employees
+        List<string> employees = new List<string>
+        {
+            "John Smith",
+            "Jackie Jackson",
+            "Chris Jones",
+            "Amanda Cullen",
+            "Jeremy Goodwin"
+        };
+
+        // Display the employee list
+        Console.WriteLine($"There are {employees.Count} employees:");
+        foreach (string employee in employees)
+        {
+            Console.WriteLine(employee);
+        }
+
+        // Ask the user for a name to remove
+        Console.Write("\nEnter an employee name to remove: ");
+        string nameToRemove = Console.ReadLine();
+
+        // Check if the name exists in the list
+        if (employees.Contains(nameToRemove))
+        {
+            employees.Remove(nameToRemove);
+            Console.WriteLine($"\nThere are {employees.Count} employees:");
+            foreach (string employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Employee not found.");
+        }
     }
+}
+
 }
